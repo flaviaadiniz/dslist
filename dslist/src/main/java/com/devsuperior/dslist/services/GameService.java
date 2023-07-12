49e07795acedde45faead1aea24d9ea6c1a957d4
flaviaadiniz <flaviaadiniz@gmail.com>
2023-07-12 09:55:25 -0300

    Add Controller, Service and Repository layers

---
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+}
