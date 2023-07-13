36808096e1c6c1eaaa80edd8b13ef4e937c805e3
flaviaadiniz <flaviaadiniz@gmail.com>
2023-07-12 21:50:39 -0300

    Add GameList entity and DTO classes

---
+++ dslist/src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public GameList() {
+
+    }
+
+    public GameList(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
