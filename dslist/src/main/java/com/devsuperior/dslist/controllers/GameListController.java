06a294b0bb759d011d8a42419835704200dd79ce
flaviaadiniz <flaviaadiniz@gmail.com>
2023-07-12 21:51:56 -0300

    Add Controller, Service and Repository layers for GameList

---
+++ dslist/src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslist.controllers;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.services.GameListService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+    private final GameListService gameListService;
+
+    public GameListController(GameListService gameListService) {
+        this.gameListService = gameListService;
+    }
+
+    @GetMapping
+    public List<GameListDTO> findAll() {
+        return gameListService.findAll();
+    }
+
+}
---
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    private final GameListRepository gameListRepository;
+
+    public GameListService(GameListRepository gameListRepository) {
+        this.gameListRepository = gameListRepository;
+    }
+
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+}
